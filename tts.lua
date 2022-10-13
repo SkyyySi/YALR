41ede609da46384a6985f3e9b64a58f294e21fb9
SkyyySi <rasti.b@protonmail.com>
2022-10-13 22:56:34 +0200

    first commit

---
+++ tts.lua
@@ -0,0 +1,214 @@
+--- Pro tip: You can also use this as the actual string conversion used
+--- by a table using metaprogramming, like this:
+--[[
+local table_to_string = require("table_to_string")
+
+local mytable, mt = {}, {}
+mt.__tostring = table_to_string
+setmetatable(mytable, mt)
+--]]
+
+local math = math
+local setmetatable = setmetatable
+local type = type
+local tostring = tostring
+local pairs = pairs
+
+local tts = {
+	-- Contains format strings
+	prettify = {
+		style = {
+			string = '\x1b[33m"%s"\x1b[0m',
+			number = "\x1b[33m%s\x1b[0m",
+			func = "\x1b[35;1m%s\x1b[0m",
+			thread = "\x1b[35;1m%s\x1b[0m", -- coroutine
+			userdata = "\x1b[36;1m%s\x1b[0m",
+			table = {
+				---@type string[] An array of colors that will be cycled through
+				bracket = { "\x1b[35m%s\x1b[0m", "\x1b[32m%s\x1b[0m", "\x1b[36m%s\x1b[0m", },
+				indent = "\x1b[2m|---\x1b[0m",
+			},
+		},
+	},
+	util = {},
+	mt = {
+		__call = function(self, ...)
+			return self.tts(...)
+		end
+	},
+}
+
+do
+	local mt = {}
+	function mt.just_return(v)
+		return v
+	end
+
+	function mt:__index(k)
+		return mt.just_return
+	end
+
+	function mt:__call(v)
+		return self[type(v)](v)
+	end
+
+	setmetatable(tts.prettify, mt)
+end
+
+tts.mt.__index = tts.mt
+setmetatable(tts, tts.mt)
+
+--- Replace escape sequences with their litteral representation.
+---@param s string
+---@return string, integer
+function tts.util.string_escape(s)
+	return s:gsub("\a", [[\a]])
+		:gsub("\b", [[\b]])
+		:gsub("\f", [[\f]])
+		:gsub("\n", [[\n]])
+		:gsub("\r", [[\r]])
+		:gsub("\t", [[\t]])
+		:gsub("\v", [[\v]])
+		:gsub("\\", [[\\]])
+		:gsub("\"", [[\"]])
+		:gsub("\'", [[\']])
+end
+
+---@param s string
+---@return string, integer
+function tts.util.string_escape(s)
+	return s:gsub("\\", [[\\]])
+		:gsub("\a", [[\a]])
+		:gsub("\b", [[\b]])
+		:gsub("\f", [[\f]])
+		:gsub("\n", [[\n]])
+		:gsub("\r", [[\r]])
+		:gsub("\t", [[\t]])
+		:gsub("\v", [[\v]])
+		:gsub("\"", [[\"]])
+		:gsub("\'", [[\']])
+end
+
+---@param str string
+---@param n number
+---@return string
+function tts.util.string_multiply(str, n)
+	local outs = ""
+	local floor = math.floor(n)
+	local point = n - floor
+
+	if n > 0 then
+		for i = 1, n do
+		outs = outs..str
+		end
+	end
+
+	if point > 0 then
+		local len = #str * floor
+		outs = outs..str:sub(1, math.floor(len))
+	end
+
+	return outs
+end
+
+--- Cycle through a table
+function tts.util.cycle(tb, i)
+	return tb[((i - 1) % #tb) + 1]
+end
+
+local function bracket_color_inc(args, bracket)
+	local ret = tts.util.cycle(tts.prettify.style.table.bracket, args.bracket_depth):format(bracket)
+	args.bracket_depth = args.bracket_depth + 1
+	return ret
+end
+
+local function bracket_color_dec(args, bracket)
+	args.bracket_depth = args.bracket_depth - 1
+	return tts.util.cycle(tts.prettify.style.table.bracket, args.bracket_depth):format(bracket)
+end
+
+-- -@param t table
+-- -@param indent? string
+-- -@param depth? integer
+-- -@param parrent? table
+-- -@return string
+--function tts.tts(t, indent, depth, parrent)
+---@param args {table: table, indent: string?, depth: integer?, parrent: table?, bracket_depth: integer?}
+---@return string
+function tts.tts(args)
+	args.table = args.table
+	args.depth = args.depth or 0
+	args.bracket_depth = args.bracket_depth or 1
+	args.parrent = args.parrent
+
+	local bracket_indent = tts.util.string_multiply(tts.prettify.style.table.indent, args.depth)
+	local full_indent = bracket_indent..tts.prettify.style.table.indent
+
+	local outs = bracket_color_inc(args, "{").."\n"
+
+	for k, v in pairs(args.table) do
+		local tv = type(v)
+		local tk = type(k)
+
+		if tv == "table" then
+			--- Prevent infinite recursion
+			if args.table == args.parrent then
+				return full_indent.."..."
+			end
+
+			v = tts.tts {
+				table = v,
+				depth = args.depth + 1,
+				bracket_depth = args.bracket_depth + 1,
+				parrent = args.table
+			}
+		else
+			v = tts.prettify[tv](v)
+		end
+
+		outs = ("%s%s%s%s%s = %s,\n"):format(outs, full_indent, bracket_color_inc(args, "["), tts.prettify[tk](k), bracket_color_dec(args, "]"), v, args.table)
+	end
+
+	return outs..bracket_indent..bracket_color_dec(args, "}")
+end
+
+---@param str string
+function tts.prettify.string(str)
+	return (tts.prettify.style.string):format(tts.util.string_escape(str))
+end
+
+---@param num number
+function tts.prettify.number(num)
+	return "\x1b[34m"..tostring(num).."\x1b[00m"
+end
+
+---@param func function
+function tts.prettify.func(func)
+	return tts.prettify.style.func:format(func)
+end
+
+---@param th thread
+function tts.prettify.userdata(th)
+	return tts.prettify.style.thread:format(th)
+end
+
+---@param ud userdata
+function tts.prettify.userdata(ud)
+	return tts.prettify.style.userdata:format(ud)
+end
+
+---@param tb table
+function tts.prettify.table(tb, ...)
+	local mt = getmetatable(tb)
+
+	--- If a table already has a string conversion, we should just
+	--- use that, because this can mean that you probably should not
+	--- blindly print its contents
+	if mt and mt.__call then
+		return tostring(tb)
+	end
+
+	return tts(tb, ...)
+end
+
+return tts
---
+++ yalr.lua
@@ -0,0 +1,112 @@
+#!/usr/bin/env lua
+local io = io
+local load = load
+local print = print
+local tostring = tostring
+local setmetatable = setmetatable
+
+local tts = require("tts")
+
+local count = 0
+
+---@param format string
+local function printf(format, ...)
+	io.stdout:write(format:format(...))
+end
+
+local line_history = {}
+
+local function read_line()
+	local line = ""
+	local special = 0
+	local hist_index = #line_history + 1
+
+	while true do
+		local c, c_byte
+		os.execute("stty -icanon")
+		c = io.read(1)
+		c_byte = string.byte(c)
+
+		if c:match("[\n]") then
+			os.execute("stty icanon")
+			table.insert(line_history, line)
+			return line
+		elseif c_byte == 27 then
+			special = 1
+		elseif c_byte == 127 then -- backspace
+			printf("\b\b   \b\b\b")
+			if #line > 0 then
+				printf("\b \b")
+				line = line:sub(1, #line-1)
+			end
+		else
+			if 0 < special and special < 2 then
+				-- ^[[A = up
+				-- ^[[B = down
+				-- ^[[C = right
+				-- ^[[D = left
+				printf("\b\b\b    \b\b\b")
+				special = special + 1
+			elseif special < 3 then
+				printf("\b\b\b    \b\b\b")
+				special = special + 1
+			else
+				line = line..tostring(c)
+			end
+			printf("{'%s' %s}_-_-_", c, c_byte)
+		end
+	end
+end
+
+--local input = read_line()
+--printf("\nYou typed: '%s'.\n", input)
+
+--[ [
+print("Welcome to YALR!")
+while true do
+	---@type string
+	local input
+	---@type function?
+	local expr
+	---@type string
+	local resoult
+	---@type any
+	local evaled_expr
+	---@type string
+	local type_of_evaled_expr
+	---@type boolean
+	local success
+
+	local err
+
+	count = count + 1
+
+	io.write("\x1b[32;1m"..tostring(count).." >>>\x1b[0m ")
+
+	success, input = pcall(read_line)
+
+	if not success then
+		print(input)
+	elseif input ~= nil then
+		if input == "exit" or input == "quit" then
+			print("Bye!")
+			os.exit(0)
+			break
+		end
+
+		success, resoult = pcall(load(input))
+		if success then
+			if resoult ~= nil then
+				print(tts.prettify(resoult))
+			end
+		else
+			success, resoult = pcall(load("return "..input))
+			if success then
+				print(tts.prettify(resoult))
+			elseif resoult ~= nil then
+				print(resoult)
+			end
+		end
+	end
+end
+--]]
